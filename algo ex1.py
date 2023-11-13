0ae6108cdb1c443edd19996dc2d2fff09aa8f31d
SOULAIMANKAROMI <147172168+soulaimankaromi@users.noreply.github.com>
2023-11-13 23:33:22 +0100

    Add files via upload

---
+++ algo ex1.py
@@ -0,0 +1,11 @@
+def is_leap_year(A):
+    if A % 400 == 0:
+        return True
+    else:
+        return False
+def leap_years_between(start_year):
+    for i in range(start_year, start_year + 100):
+        if is_leap_year(i) == True:
+            print(i, "is a leap year.")
+start_year=int(input("type the start year"))
+leap_years_between(start_year)
